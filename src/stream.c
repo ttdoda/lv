b79149d55b8c51c485a955a2f978f44385c73737
IWAMOTO Kouichi <sue@iwmt.org>
2016-06-06 14:44:56 +0900

    add xz/lzma support.

--- src/stream.c
+++ src/stream.c
@@ -43,6 +43,8 @@
 
 private byte *gz_filter = "zcat";
 private byte *bz2_filter = "bzcat";
+private byte *xz_filter = "xzcat";
+private byte *lzma_filter = "lzcat";
 
 private stream_t *StreamAlloc()
 {
@@ -75,6 +77,10 @@ public stream_t *StreamOpen( byte *file )
       filter = gz_filter;
     else if( !strcmp( "bz2", exts ) || !strcmp( "BZ2", exts ) )
       filter = bz2_filter;
+    else if( !strcmp( "xz", exts ) || !strcmp( "XZ", exts ) )
+      filter = xz_filter;
+    else if( !strcmp( "lzma", exts ) || !strcmp( "LZMA", exts ) )
+      filter = lzma_filter;
   }
   if( NULL != filter ){
     /*
